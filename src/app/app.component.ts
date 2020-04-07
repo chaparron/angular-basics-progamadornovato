23743e314881e9dc6eeb78224d67eaf6aba0929b
Chaparron <chaparron@github.com>
2020-04-07 12:12:04 +0200

    curso de programador novato con las cosas básicas de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular';
+  title = 'aprendiendo angular';
+  mostrar = true;
+  lenguajes:string[]=['js','ts','java'];
+  frameworks:Array<any>=[
+    {nombre:'Angular',pop:'70%', edad:9},
+    {nombre:'React',pop:'50%', edad:5},
+    {nombre:'Vuejs',pop:'20%', edad:5},
+    
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FrameworkService } from './framework.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { PieComponent } from './pie/pie.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { UbicacionComponent } from './ubicacion/ubicacion.component';
+import { E404Component } from './e404/e404.component';
+import { FrameworkComponent } from './framework/framework.component';
+
+const routes: Routes = [
+  { path: 'formulario', component: FormularioComponent },
+  { path: 'ubicacion', component: UbicacionComponent },
+  { path: 'framework/:id', component: FrameworkComponent },
+  { path: '404', component: E404Component },
+  { path: '', component: InicioComponent },
+  { path: '**', redirectTo: '/404', pathMatch:'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    PieComponent,
+    FormularioComponent,
+    InicioComponent,
+    UbicacionComponent,
+    E404Component,
+    FrameworkComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [FrameworkService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
